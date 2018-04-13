6ba1f8a61641c58ad856120a26c463ac269fab41
Emil Lindblom <emillindblom95@gmail.com>
2018-04-14 01:10:47 +0200

    Add stuff to content component

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import './styles.css'
 
-function Content() {
-  return (
-    <div className='content'>
-      Content component
-    </div>
-  )
+class Content extends React.Component {
+  constructor (props) {
+    super(props)
+  }
+
+  render () {
+    let {title, date, summary, href} = this.props
+    return (
+      <div className='content'>
+        <h1>{title}</h1>
+        <h5>{date}</h5>
+        <p>{summary}</p>
+        <a href={href} target='_blank'>Se hela motionen</a>
+      </div>
+    )
+  }
 }
 
 export default Content
