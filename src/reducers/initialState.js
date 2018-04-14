0e062f081d82b547b8fc96e428d3ff57514b9526
Albin Remnestål <albin.remnestal@gmail.com>
2018-04-14 11:49:39 +0200

    add more data to the initial state of the sidebar

--- src/reducers/initialState.js
+++ src/reducers/initialState.js
@@ -8,6 +8,31 @@ export default {
         'date': '2018-04-13 23:15:12.997344',
         'href': 'http://www.riksdagen.se/sv/dokument-lagar/dokument/motion/reformer-for-fler-bostader_H3022702',
         'summary': 'Riksdagen ställer sig bakom det som anförs i motionen om värdet av det bostadspolitiska målet om långsiktigt väl fungerande bostadsmarknader där konsumenternas efterfrågan möter ett utbud av bostäder som svarar mot behoven och tillkännager detta för regeringen. Riksdagen ställer sig bakom det som anförs i motionen om att bostadspolitiken särskilt ska värna långsiktiga spelregler för att främja välfungerande bygg- och bostadsmarknader och tillkännager detta för regeringen. Riksdagen ställer sig bakom det som anförs i motionen om att göra en översyn av befintlig forskning om effekterna av bostadssubventioner och tillkännager detta för regeringen.'
+      },
+      {
+        'id': 1,
+        'title': 'Motion 2017/18:3900',
+        'peek': 'Reformer för bostäder 2',
+        'date': '2018-04-13 23:15:12.997344',
+        'href': 'http://www.riksdagen.se/sv/dokument-lagar/dokument/motion/reformer-for-fler-bostader_H3022702',
+        'summary': 'Riksdagen ställer sig bakom det som anförs i motionen om värdet av det bostadspolitiska målet om långsiktigt väl fungerande bostadsmarknader där konsumenternas efterfrågan möter ett utbud av bostäder som svarar mot behoven och tillkännager detta för regeringen. Riksdagen ställer sig bakom det som anförs i motionen om att bostadspolitiken särskilt ska värna långsiktiga spelregler för att främja välfungerande bygg- och bostadsmarknader och tillkännager detta för regeringen. Riksdagen ställer sig bakom det som anförs i motionen om att göra en översyn av befintlig forskning om effekterna av bostadssubventioner och tillkännager detta för regeringen.'
+      },
+      {
+        'active': true,
+        'id': 2,
+        'title': 'Motion 2017/18:3989',
+        'peek': 'Reformer för bostäder 3',
+        'date': '2018-04-13 23:15:12.997344',
+        'href': 'http://www.riksdagen.se/sv/dokument-lagar/dokument/motion/reformer-for-fler-bostader_H3022702',
+        'summary': 'Riksdagen ställer sig bakom det som anförs i motionen om värdet av det bostadspolitiska målet om långsiktigt väl fungerande bostadsmarknader där konsumenternas efterfrågan möter ett utbud av bostäder som svarar mot behoven och tillkännager detta för regeringen. Riksdagen ställer sig bakom det som anförs i motionen om att bostadspolitiken särskilt ska värna långsiktiga spelregler för att främja välfungerande bygg- och bostadsmarknader och tillkännager detta för regeringen. Riksdagen ställer sig bakom det som anförs i motionen om att göra en översyn av befintlig forskning om effekterna av bostadssubventioner och tillkännager detta för regeringen.'
+      },
+      {
+        'id': 3,
+        'title': 'Motion 2017/18:3988',
+        'peek': 'Reformer för bostäder 4',
+        'date': '2018-04-13 23:15:12.997344',
+        'href': 'http://www.riksdagen.se/sv/dokument-lagar/dokument/motion/reformer-for-fler-bostader_H3022702',
+        'summary': 'Riksdagen ställer sig bakom det som anförs i motionen om värdet av det bostadspolitiska målet om långsiktigt väl fungerande bostadsmarknader där konsumenternas efterfrågan möter ett utbud av bostäder som svarar mot behoven och tillkännager detta för regeringen. Riksdagen ställer sig bakom det som anförs i motionen om att bostadspolitiken särskilt ska värna långsiktiga spelregler för att främja välfungerande bygg- och bostadsmarknader och tillkännager detta för regeringen. Riksdagen ställer sig bakom det som anförs i motionen om att göra en översyn av befintlig forskning om effekterna av bostadssubventioner och tillkännager detta för regeringen.'
       }
     ]
   },
