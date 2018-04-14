aa76d72e23da7ea6783c2d0c7c918b613589a8c3
Jonathan Berglind <jonathan.berglind@gmail.com>
2018-04-14 10:29:47 +0200

    Add api.js

---
+++ src/api.js
@@ -0,0 +1,35 @@
+const BASE_URL = process.env.API_BASE_URL || 'http://localhost:3000'
+const STATUS_OK = 200
+
+const jsonHeader = {
+  'Content-Type': 'application/json'
+}
+
+function checkStatus(response) {
+  if (response.status >= STATUS_OK && response.status < 300) {
+    return Promise.resolve(response)
+  } else {
+    return Promise.reject(response)
+  }
+}
+
+function parseJSON(response) {
+  return response.json()
+}
+
+function ftch(...args) {
+  return fetch(...args)
+    .then(checkStatus)
+    .then(parseJSON)
+}
+
+// Returns a Promise with the data of /motions
+module.exports.getMotions = function() {
+  const url = `${BASE_URL}/motions`
+  return ftch(
+    url, {
+      method: 'GET',
+      headers: { ...jsonHeader }
+    }
+  )
+}
